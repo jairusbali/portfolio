4c2e5ec1d5485ed1c48bc234caddae3640d8172a
jairus <jairus.bali@gmail.com>
2019-05-14 14:49:14 -0400

    html skeleton and rough layout implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { NavBar, Header, About, Projects, Contact } from "./components/";
 
-function App() {
+export default props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <NavBar />
+      <Header />
+      <About />
 
-export default App;
+      <Projects />
+      <Contact />
+    </React.Fragment>
+  );
+};

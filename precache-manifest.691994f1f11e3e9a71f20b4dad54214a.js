73e736d94c0fafaf80c0aee77f2d53da5d62a8bf
jairus <jairus.bali@gmail.com>
2019-05-16 16:32:43 -0400

    Updates

---
+++ precache-manifest.691994f1f11e3e9a71f20b4dad54214a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7421161ce8836c430178f5aea3147305",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bdf73e4f816adba260f4",
+    "url": "/portfolio/static/css/main.887b2e99.chunk.css"
+  },
+  {
+    "revision": "29cd202c29b57a0f9540",
+    "url": "/portfolio/static/js/2.bdd24b50.chunk.js"
+  },
+  {
+    "revision": "bdf73e4f816adba260f4",
+    "url": "/portfolio/static/js/main.c9d0747b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
